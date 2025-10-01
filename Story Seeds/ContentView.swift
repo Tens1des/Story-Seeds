cfe126ec3bbb18002aa72b4811a7e7f39ba74fc7
Tens1des <romankotov3213@gmail.com>
2025-10-01 21:54:36 +0300

    Initial Commit

---
+++ Story Seeds/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Story Seeds
+//
+//  Created by Рома Котов on 01.10.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Story Seeds/Story_SeedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Story_SeedsApp.swift
+//  Story Seeds
+//
+//  Created by Рома Котов on 01.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct Story_SeedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
